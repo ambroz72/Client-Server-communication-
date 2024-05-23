64600d3fdab53e9c940307e725d258c9aefafac9
Abin Varghese <97914997+ambroz72@users.noreply.github.com>
2024-05-23 22:31:35 +0530

    2.py

--- client.py
+++ client.py
@@ -27,9 +27,5 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 HOST_NAME=socket.gethostname()
 PORT=12345
 s.connect((HOST_NAME,PORT))
-# while True:
-#     msg=s.recv(100)
-#     print('Server:'+msg.decode('utf-8'))
-#     message_to_send=input('Client:')
 root.mainloop()
-    #print(msg.decode('utf-8'))
+ 
